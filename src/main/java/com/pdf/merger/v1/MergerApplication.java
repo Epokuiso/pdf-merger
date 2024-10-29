25417587e4a7e68c2ae91af41497adc42b95de97
Epokuiso <epokuiso@gmail.com>
2024-10-29 10:15:51 +0100

    initial commit

---
+++ src/main/java/com/pdf/merger/v1/MergerApplication.java
@@ -0,0 +1,13 @@
+package com.pdf.merger.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MergerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MergerApplication.class, args);
+	}
+
+}
